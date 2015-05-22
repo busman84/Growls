e1d60944309bbc2809766e5116d1fbdbd4bc9483
Walker Flieller <wflieller@gmail.com>
2015-05-22 17:09:07 -0500

    Nav bar page functions

--- app.rb
+++ app.rb
@@ -1,12 +1,34 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
+
 get '/' do
 	
 	@growl = params[:growl]
 
 	erb :index
+	
 end
+
+get '/login' do
+	
+	erb:login
+	
+end
+
+get '/growls' do
+	
+	erb:growls
+end
+
+get '/about' do
+	
+	erb:about
+	
+end
+
+
 
 
 
